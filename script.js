28335ffb33521d5702fa270ce40e625892c8dec6
miguelceccon-gif <miguel.ceccon@escola.pr.gov.br>
2026-08-13 09:55:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const botoes = document.querySelectorAll("button");
 
+botoes.forEach(function (botao) {
+    let curtiu = false;
+    botao.addEventListener("click", botaoClicado);
+
+    function botaoClicado() {
+        let texto = botao.querySelector("span");
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
+            botao.style.backgroundColor = "#0056b3"; // Muda a cor ao clicar
+        } else {
+            texto.textContent--;
+            curtiu = false;
+            botao.style.backgroundColor = "#001f3f"; // Volta a cor original
+        }
+    }
+})
